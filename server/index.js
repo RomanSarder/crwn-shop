4b1d0395a8f2f338136dd45670d090aab67122c9
RomanSarder <roman.sarder@yandex.ru>
2021-04-16 00:56:47 +0300

    Enforce https on express Register service worker

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const path = require('path')
 const compression = require('compression')
+const enforce = require('express-sslify')
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(compression())
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
+
+app.get('/service-worker.js', function(req, res) {
+    res.sendFile(path.resolve(__dirname, '..', 'build/service-worker.js'))
+})
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import * as serviceWorker from './serviceWorkerRegistration'
+
 import { BrowserRouter } from 'react-router-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -25,3 +28,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+serviceWorker.register()
